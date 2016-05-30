383b4afb4a903b02fb1ed8ae9e007f1a77cc4493
Jean-Pierre Gygax <gygax@practicomp.ch>
2016-05-30 13:44:35 +0200

    libGPCFonts is now a Conan dependency

--- rasterizer/conanfile.py
+++ rasterizer/conanfile.py
@@ -4,15 +4,13 @@ class GPCFontRasterizerConan(ConanFile):
   name = "gpcfontrasterizer"
   version = "0.1"
   settings = "os", "compiler", "build_type", "arch"
-  requires = "freetype/2.6.3@lasote/stable", "Boost/1.60.0@lasote/stable"
+  requires = "freetype/2.6.3@lasote/stable", "Boost/1.60.0@lasote/stable", "libGPCFonts/0.1@jpgygax68/alpha", "cereal/0.1@jpgygax68/testing"
   generators = "cmake"
   exports = "main.cpp", "CMakeLists.txt", "cmake"
   default_options = "Boost:shared=False"
 
   def build(self):
     cmake = CMake(self.settings)
-    #self.run("conan install .")
-    self.run("git submodule update")
     self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
     self.run("cmake --build . %s" % cmake.build_config)
 
