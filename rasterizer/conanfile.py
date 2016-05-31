6b7dce873c6df92b963355a626c9697c618de0dc
Jean-Pierre Gygax <gygax@practicomp.ch>
2016-05-31 11:58:09 +0200

    zlib is no longer a submodule, now using Conan to obtain it

--- rasterizer/conanfile.py
+++ rasterizer/conanfile.py
@@ -4,7 +4,7 @@ class GPCFontRasterizerConan(ConanFile):
   name = "gpcfontrasterizer"
   version = "0.1"
   settings = "os", "compiler", "build_type", "arch"
-  requires = "freetype/2.6.3@lasote/stable", "Boost/1.60.0@lasote/stable", "libGPCFonts/0.1@jpgygax68/alpha", "cereal/0.1@jpgygax68/testing"
+  requires = "freetype/2.6.3@lasote/stable", "Boost/1.60.0@lasote/stable", "libGPCFonts/0.1@jpgygax68/alpha", "cereal/0.1@jpgygax68/testing", "zlib/1.2.8@lasote/stable"
   generators = "cmake"
   exports = "main.cpp", "CMakeLists.txt", "cmake"
   default_options = "Boost:shared=False"
